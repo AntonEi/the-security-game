379c978fad05473840a8939b5171e6a02adfa202
Aaren Bertilsson <aarenbs@gmail.com>
2026-06-08 19:28:30 +0200

    Add temp text to email1

--- rooms/Room-PhishingMail.ps1
+++ rooms/Room-PhishingMail.ps1
@@ -47,16 +47,14 @@ function Start-Room {
 
 Function Show-Email1 {
     Write-Host "========================================================"
-    Write-Host "Before you is a mailbox, filled with 3 emails:"
-    Write-Host "1 -  Important notice"
-    Write-Host "2 - [External] [Urgent]: Domain Renewal Failure - Mon 13.."
-    Write-Host "3 - Someone tried to log in to your account"
+    Write-Host "Important notice"
+    Write-Host "From: example@mail.com"
+    Write-Host "Text text text"
+    Write-Host "<link> Click here to stop removal"
     Write-Host "========================================================"
     Write-Host ""
-    Write-Host "1. Look at email 1"
-    Write-Host "2. Look at email 2 "
-    Write-Host "3. Look at email 3"
-    Write-Host "4. Hint"
+    Write-Host "1. Click on the link"
+    Write-Host "2. Go back"
     Write-Host ""
 }
 
