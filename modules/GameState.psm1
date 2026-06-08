160125e75c8a9af75a67dd5f027ec4a58d46f18e
Anton Ei <Antonake@icloud.com>
2026-06-08 09:06:46 +0200

    Add save game state function

--- modules/GameState.psm1
+++ modules/GameState.psm1
@@ -14,3 +14,24 @@ function New-GameState {
         CompletedRooms = @()
     }
 }
+
+function Save-GameState {
+    param (
+        [Parameter(Mandatory = $true)]
+        [object]$GameState,
+
+        [string]$SavePath = "$PSScriptRoot/../saves/savegame.json"
+    )
+
+    $saveDirectory = Split-Path -Path $SavePath -Parent
+
+    if (-not (Test-Path -Path $saveDirectory)) {
+        New-Item -Path $saveDirectory -ItemType Directory | Out-Null
+    }
+
+    $GameState |
+        ConvertTo-Json -Depth 5 |
+        Set-Content -Path $SavePath -Encoding UTF8
+}
+
+Export-ModuleMember -Function New-GameState, Save-GameState
