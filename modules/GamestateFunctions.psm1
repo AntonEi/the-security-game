f1a24d2add40d2b85aa859bcaa9a069b6f88c184
gottosson <gabriellaottosson@hotmail.se>
2026-06-11 11:12:02 +0200

    issue 8 skapa poängsystem (#32)
    * Removed Update-Score function as per new scoring system
    
    * Added Add-Score function
    
    * Added Remove-Score function
    
    * Changed place for Remove-Score function
    
    * Added difficulty modifier structure to Add-Score
    
    * Added difficulty modifier structure to Remove-Score

--- modules/GamestateFunctions.psm1
+++ modules/GamestateFunctions.psm1
@@ -1,16 +1,40 @@
 # Module for handling GameState updates such as score, room progression, hints and mistakes
 
-# Function for updating the player's score by adding the specified number of points
-function Update-Score {
+# Function for adding score for a correct answer
+function Add-Score {
     param (
         [Parameter(Mandatory = $true)]
-        [object]$GameState,
-
-        [Parameter(Mandatory = $true)]
-        [int]$Points
+        [object]$GameState
     )
+    # Default modifier (future difficulty scaling)
+    $modifier = 1
 
-    $GameState.Score += $Points
+    switch ($GameState.Difficulty) {
+        "Easy"   { $modifier = 1 }   # future: maybe 0.5
+        "Medium" { $modifier = 1 }   # future: stays 1
+        "Hard"   { $modifier = 1 }   # future: maybe 1.5
+    }
+
+    $GameState.Score += (20 * $modifier)
+    return $GameState
+}
+
+# Function for removing score for an incorrect answer
+function Remove-Score {
+    param (
+        [Parameter(Mandatory = $true)]
+        [object]$GameState
+    )
+    # Default modifier (future difficulty scaling)
+    $modifier = 1
+
+    switch ($GameState.Difficulty) {
+        "Easy"   { $modifier = 1 }   # future: maybe 0.5
+        "Medium" { $modifier = 1 }   # future: stays 1
+        "Hard"   { $modifier = 1 }   # future: maybe 2
+    }
+
+    $GameState.Score -= (5 * $modifier)
     return $GameState
 }
 
@@ -67,4 +91,4 @@ function Complete-Room {
     return $GameState
 }
 
-Export-ModuleMember -Function Update-Score, Set-CurrentRoom, Use-Hint, Add-Mistake, Complete-Room
+Export-ModuleMember -Function Add-Score, Remove-Score, Set-CurrentRoom, Use-Hint, Add-Mistake, Complete-Room
