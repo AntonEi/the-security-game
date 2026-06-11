b5c1bd649e520b93c60250b3d13e51169d334d05
Aaren Bertilsson <aarenbs@gmail.com>
2026-06-11 17:47:51 +0200

    Add base for teams invite room

---
+++ rooms/Room-TeamsInvite.ps1
@@ -0,0 +1,48 @@
+# Main function for playing the teams invite room
+
+Function Start-TeamsInviteRoom {
+    param (
+        [Parameter(Mandatory = $true)]
+        [object]$GameState
+    )
+
+    while ($true) {
+        Show-TerminalBox -Label "ROOM 0X" -Lines @( # TODO: idk what room number this actually is
+            "You open up teams, and notice a new message request.",
+            "",
+            "from: itservisprovider@onmicrosoft.com",
+            "Service Internet provider (Extrernal) wants to chat with you!",
+            "! This person is from outside your organisation",
+            "Messages from unknown or unexpected people could be spam or phising attempts.",
+            "Never share your account information or authorize sign-in requests over chat.",
+            "To be safe, preview their messages",
+            "",
+            "What will you do?"
+            ) -BorderColor "Cyan" -TextColor "White" -Clear
+
+            $choice = Read-Host "1: preview the message, 2: block, 3: accept or HINT"
+
+            switch ($choice) {
+                "1" {
+                    # Preview the message
+
+                }
+                "2" {
+                    # Block
+
+                }
+                "3" {
+                    # Accept
+
+                }
+                "HINT" {
+
+                }
+                default {
+                    Write-Host "Invalid choice. Please try again." -ForegroundColor Red
+                    Start-Sleep -Seconds 2
+                }
+            }    
+    }
+
+}
